71e80f8aef76cc3fca103f055abe2d390cf2b999
Lora Johns <lora.r.johns@gmail.com>
2020-04-23 01:20:49 -0400

    removed unused imports from healthcheck

--- resources/healthcheck.py
+++ resources/healthcheck.py
@@ -2,8 +2,6 @@ import logging
 from flask_jsonpify import jsonify
 from flask_restful import Resource, reqparse, inputs
 from flask import make_response
-from werkzeug.datastructures import FileStorage
-
 
 class HealthCheck(Resource):
     def get(self):
